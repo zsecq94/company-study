dd66f4ae16ad70dca83256d6bff5c62c614e5203
zsecq94 <zsecq94@gmail.com>
2024-01-16 16:07:06 +0900

    feat : 대소문자 test

--- new/src/js/main.js
+++ new/src/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  // 슬라이더 영역
   // $(".slide:gt(0)").hide();
   const $timeSpan = $(".slide-con > .slide-bot-box > .timer-box > .cur");
   const $timerLine = $(".slide-con > .slide-bot-box > .timer-box > .timer-line > .time");
@@ -53,4 +54,136 @@ $(document).ready(() => {
 
   setInterval(currentTime, 10);
   let interval = setInterval(nextSlide, 6000);
+
+  // content 영역 //
+  const contents = [
+    {
+      $prevBtn: $(".content1-con > .title-box > .nav-box > .prev"),
+      $nextBtn: $(".content1-con > .title-box > .nav-box > .next"),
+      $contentCon: $(".content1-con > .hidden > .card-con"),
+      translate: 16.9,
+      translateXs: 0,
+      translateIdx: 0,
+    },
+    {
+      $prevBtn: $(".content2-con > .title-box > .nav-box > .prev"),
+      $nextBtn: $(".content2-con > .title-box > .nav-box > .next"),
+      $contentCon: $(".content2-con > .hidden > .card-con"),
+      translate: 20.3,
+      translateXs: 0,
+      translateIdx: 0,
+    },
+    {
+      $prevBtn: $(".content4-con > .title-box > .nav-box > .prev"),
+      $nextBtn: $(".content4-con > .title-box > .nav-box > .next"),
+      $contentCon: $(".content4-con > .hidden > .card-con"),
+      translate: 16.9,
+      translateXs: 0,
+      translateIdx: 0,
+    },
+  ];
+
+  contents.forEach((content, index) => {
+    let isDragging = false;
+    let startX = 0;
+    let initialTranslateXs = 0;
+
+    const contentBtnStyleUpdate = () => {
+      if (content.translateIdx <= 0) {
+        content.$prevBtn.addClass("disabled");
+      } else if (content.translateIdx >= 2) {
+        content.$nextBtn.addClass("disabled");
+      } else {
+        content.$prevBtn.removeClass("disabled");
+        content.$nextBtn.removeClass("disabled");
+      }
+    };
+
+    content.$prevBtn.click(() => {
+      if (content.translateIdx > 0) {
+        content.translateXs -= content.translate;
+        content.translateIdx--;
+        content.$contentCon.css("transform", `translateX(-${content.translateXs}%)`);
+      }
+      contentBtnStyleUpdate();
+    });
+
+    content.$nextBtn.click(() => {
+      if (content.translateIdx < 2) {
+        content.translateXs += content.translate;
+        content.translateIdx++;
+        content.$contentCon.css("transform", `translateX(-${content.translateXs}%)`);
+      }
+      contentBtnStyleUpdate();
+    });
+
+    contentBtnStyleUpdate();
+
+    if (index === 1) {
+      content.$contentCon.on("touchstart", (e) => {
+        startPoint = e.touches[0].pageX;
+      });
+      content.$contentCon.on("touchend", (e) => {
+        endPoint = e.changedTouches[0].pageX;
+        if (startPoint < endPoint && content.translateIdx > 0) {
+          content.translateXs -= 20;
+          content.translateIdx--;
+          circleStyleUpdate();
+          content.$contentCon.css("transform", `translateX(-${content.translateXs}%)`);
+        } else if (startPoint > endPoint && content.translateIdx < 4) {
+          content.translateXs += 20;
+          content.translateIdx++;
+          circleStyleUpdate();
+          content.$contentCon.css("transform", `translateX(-${content.translateXs}%)`);
+        }
+      });
+    } else {
+      content.$contentCon.on("touchstart", function (e) {
+        isDragging = true;
+        startX = e.originalEvent.touches[0].clientX;
+        initialTranslateXs = content.translateXs;
+      });
+
+      content.$contentCon.on("touchmove", function (e) {
+        if (isDragging) {
+          const currentX = e.originalEvent.touches[0].clientX;
+          const dx = currentX - startX;
+          const percentageChange = (dx / $(this).width()) * 100;
+          content.translateXs = initialTranslateXs + percentageChange;
+
+          if (content.translateXs > 0) content.translateXs = 0;
+          if (content.translateXs < -84) content.translateXs = -84;
+
+          content.$contentCon.css("transform", `translateX(${content.translateXs}%)`);
+        }
+      });
+
+      content.$contentCon.on("touchend", function () {
+        isDragging = false;
+      });
+    }
+  });
+
+  // content2 바텀 영역
+  const $botBtn = $(".content2-con > .hidden > .bot-btn");
+
+  for (let i = 0; i < 5; i++) {
+    $botBtn.append(`<div class="circle"></div>`);
+  }
+
+  const $circle = $(".content2-con > .hidden > .bot-btn > .circle");
+  const circleStyleUpdate = () => {
+    let idx = contents[1].translateIdx;
+    $circle
+      .css({
+        border: "solid 1px " + (idx !== 0 && idx !== 4 ? "black" : "white"),
+      })
+      .each(function (index) {
+        $(this).css({
+          backgroundColor: idx === index ? (idx !== 0 && idx !== 4 ? "black" : "white") : "",
+        });
+      });
+  };
+
+  circleStyleUpdate();
 });
